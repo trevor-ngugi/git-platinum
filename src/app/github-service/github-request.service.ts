70229d78d0d5412ed195046fe7dca537a6caf360
trevor ngugi <ngugitrevor@gmail.com>
2019-12-08 21:00:55 +0300

    add promise function in our github request

--- src/app/github-service/github-request.service.ts
+++ src/app/github-service/github-request.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../../environments/environment';
+import{Users} from "../users"
+import { reject } from 'q';
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
 
-  constructor() { }
+  users:Users
+
+  constructor(private http:HttpClient) { 
+    this.users=new Users("","",0,0,0,"","","","","",new Date())
+  }
+
+  gitHubRequest(){
+    interface ApiResponse{
+       login:string,
+       avatar_url:string,
+       followers:number,
+       following:number,
+       public_repos:number,
+       bio:string,
+       location:string,
+       blog:string,
+       email:string,
+       company:string,
+       created_at:Date
+    }
+  let promise=new Promise((resolve,reject)=>{
+    this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+      this.users.bio=response.bio;
+      this.users.avatar_url=response.avatar_url;
+      this.users.login=response.login;
+      this.users.public_repos=response.public_repos;
+      this.users.created_at=response.created_at;
+      // this.users.updated_at=response.updated_at;
+      // this.users.html_url=response.html_url;
+
+      resolve()
+    },
+    error=>{
+      reject(error)
+    }
+  )
+  })
+return promise
+}
+
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,24 +13,10 @@ users:Users
   constructor(private http:HttpClient) { }
 
   ngOnInit() {
-    interface ApiResponse{
-       login:string,
-       avatar_url:string,
-       followers:number,
-       following:number,
-       public_repos:number,
-       bio:string,
-       location:string,
-       blog:string,
-       email:string,
-       company:string,
-       created_at:Date
+    
     }
 
-    this.http.get<ApiResponse>("http://api.github.com/users").subscribe(data=>{
-      // Succesful API request
-      this.users= new Users(data.login, data.avatar_url,data.followers,data.following,data.public_repos,data.bio,data.location,data.blog,data.email,data.company,data.created_at)
-    })
+    
   }
 
-}
+
