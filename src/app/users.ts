e1126408fd929de851897d6f7e71725170fd5533
trevor ngugi <ngugitrevor@gmail.com>
2019-12-08 16:37:08 +0300

    add blueprint for user class

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,16 @@
 export class Users {
+    constructor(public login:string,
+                public avatar_url:string,
+                public followers:number,
+                public following:number,
+                public public_repos:number,
+                public bio:string,
+                public location:string,
+                public blog:string,
+                public email:string,
+                public company:string,
+                public created_at:Date
+                ){
+                
+                }
 }
