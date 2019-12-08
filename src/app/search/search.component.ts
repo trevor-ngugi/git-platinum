d1cdf15ddb3459425f2be12a47178dd7d06568fd
trevor ngugi <ngugitrevor@gmail.com>
2019-12-08 20:09:00 +0300

    add http request in search ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{Users} from "../users"
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+users:Users
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    interface ApiResponse{
+       login:string,
+       avatar_url:string,
+       followers:number,
+       following:number,
+       public_repos:number,
+       bio:string,
+       location:string,
+       blog:string,
+       email:string,
+       company:string,
+       created_at:Date
+    }
+
+    this.http.get<ApiResponse>("http://api.github.com/users").subscribe(data=>{
+      // Succesful API request
+      this.users= new Users(data.login, data.avatar_url,data.followers,data.following,data.public_repos,data.bio,data.location,data.blog,data.email,data.company,data.created_at)
+    })
   }
 
 }
